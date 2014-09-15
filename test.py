1bb0e3297a33220e5d40f7f1405f1c605f2edbc9
Obbinkc <christiaan_obbink@hotmail.com>
2014-09-15 13:09:21 +0200

    test network
    python network code, crap die niet werkt..

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
-kayue = input("Ka Yue wat ben je? ")
-if kayue == "cool":
-    print ("Ka Yue je bent cool")
-else:
-    print ("Je bent toch niet zo cool")
+#!/usr/bin/python           # This is server.py file
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                   # Reserve a port for your service.
+s.bind((host, port))        # Bind to the port
+
+s.listen(5)                 # Now wait for client connection.
+while True:
+   c, addr = s.accept()     # Establish connection with client.
+   print ('Got connection from'), addr
+   c.send('Thank you for connecting')
+   c.close()                # Close the connection
